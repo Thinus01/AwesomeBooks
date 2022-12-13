47731964e47c3dba63b0d6f12947c51ad50d497f
Thinus <thinusvdv2001@gmail.com>
2022-12-13 16:45:50 +0200

    Add modules

---
+++ modules/aboutPage.js
@@ -0,0 +1,7 @@
+const clickedContact = () => {
+  document.getElementById('contacts').style.display = 'flex';
+  document.getElementById('bookfirst').style.display = 'none';
+  document.getElementById('addBook').style.display = 'none';
+};
+
+export default clickedContact;
---
+++ modules/list.js
@@ -0,0 +1,6 @@
+const addNew = () => {
+  document.getElementById('addBook').style.display = 'flex';
+  document.getElementById('bookfirst').style.display = 'none';
+  document.getElementById('contacts').style.display = 'none';
+};
+export default addNew;
